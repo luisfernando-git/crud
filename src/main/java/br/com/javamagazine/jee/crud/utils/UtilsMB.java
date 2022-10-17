e76e051ed1dcc975a6623d7fdecbe376a74ce268
vialaserdepila\luis.goncalves <luis.goncalves@vialaser.com.br>
2022-10-17 11:38:00 -0300

    Finalização projeto crud artigo DevMedia

---
+++ src/main/java/br/com/javamagazine/jee/crud/utils/UtilsMB.java
@@ -0,0 +1,14 @@
+package br.com.javamagazine.jee.crud.utils;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import java.util.TimeZone;
+
+@Named
+@ApplicationScoped
+public class UtilsMB {
+
+    public TimeZone getTimeZone() {
+        return TimeZone.getDefault();
+    }
+}
---
+++ src/test/java/br/com/javamagazine/jee/crud/common/validation/EmailValidatorTest.java
@@ -0,0 +1,14 @@
+package br.com.javamagazine.jee.crud.common.validation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmailValidatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
